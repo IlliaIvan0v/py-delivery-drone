ea88572aa2f50838ee3290949f9c51b7e172cb77
Illia Ivanov <Illuha.Iva@gmail.com>
2025-05-11 15:40:28 +0200

    fixed issues

--- app/main.py
+++ app/main.py
@@ -47,7 +47,7 @@ class DeliveryDrone(FlyingRobot):
             coords = [0, 0, 0]
         super().__init__(name, weight, coords)
         self.max_load_weight = max_load_weight
-        self.current_load = None
+        self.current_load = current_load
         if current_load:
             self.hook_load(current_load)
 
